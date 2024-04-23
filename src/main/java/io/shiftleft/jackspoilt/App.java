7b8dd37140c11494882213b106df2c600c710f7f
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-04-22 21:46:58 -0400

    Update App.java

--- src/main/java/io/shiftleft/jackspoilt/App.java
+++ src/main/java/io/shiftleft/jackspoilt/App.java
@@ -14,17 +14,18 @@ import spark.Request;
 
 public class App {
 
-  private static final Logger log = LoggerFactory.getLogger(App.class);
+  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final File logFile = new File("application.log");
 
   /*
   Trigger Gadget Chain
    */
-  private static ObjectMapper deserializer = new ObjectMapper().enableDefaultTyping();
+  private static ObjectMapper deserializer = new ObjectMapper();
   private static ObjectMapper serializer = new ObjectMapper();
   private static AccountStore accounts = new AccountStore();
 
-  public static void main(String[] args) {
-    port(8888);
+  private static void logRequest(HttpRequest request) throws IOException {
+        FileUtils.writeStringToFile(logFile, "/accounts -> " + request.body(), true);
 
     get("/accounts", (request, response) -> {
       Collection<Account> res = accounts.list();
@@ -48,13 +49,7 @@ public class App {
 
   }
 
-  private static Account deserialize(Request request)
-      throws IOException, JsonParseException, JsonMappingException {
-    try {
-      return deserializer.readValue(request.body(), Account.class);
-    } catch (Exception any) {
-      log.warn("Unexpected exception deserializing content: {}", any.getClass());
-      return null;
+  private static Account deserialize(HttpRequest request) throws IOException {
+        return mapper.readValue(request.body(), Account.class);
     }
   }
-}
